ab62f8a356f7428a9267e38aa3ba914929b59e9e
dvidaletti <dvidaletti@stefanini.com>
2018-10-15 18:36:18 -0300

    Updates

---
+++ precache-manifest.6ff65ce83eb6bc1ff7908f856eecc9d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7456dfed453aeb7d8b52e7d676c5933c",
+    "url": "/static/media/wallpaper.7456dfed.jpg"
+  },
+  {
+    "revision": "c41f740a5bf4b1e663ee3184b08677f7",
+    "url": "/static/media/star-wars-logo.c41f740a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9266aeb1b5e9567bcd02",
+    "url": "/static/js/main.9266aeb1.chunk.js"
+  },
+  {
+    "revision": "4c79c11fceb3546d2d10",
+    "url": "/static/js/1.4c79c11f.chunk.js"
+  },
+  {
+    "revision": "9266aeb1b5e9567bcd02",
+    "url": "/static/css/main.92fdf1ca.chunk.css"
+  },
+  {
+    "revision": "179799353c198f576066f24fe9250918",
+    "url": "/index.html"
+  }
+];
